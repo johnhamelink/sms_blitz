2cbb8c8cb71451ba1526a3d76e437fe9ea3adf06
John Hamelink <john@johnhamelink.com>
2016-04-04 01:29:09 +0100

    Added plivo SMS Adapter

---
+++ lib/sms_blitz/adapters/plivo.ex
@@ -0,0 +1,40 @@
+defmodule SmsBlitz.Adapters.Plivo do
+
+  @base_uri "https://api.plivo.com/v1"
+
+  def authenticate({user_id, user_token}) do
+    %{
+      uri:  Enum.join([@base_uri, "Account", user_id, "Message"], "/") <> "/",
+      auth: Base.encode64("#{user_id}:#{user_token}")
+    }
+  end
+
+  def send_sms(%{uri: uri, auth: auth}, from: from, to: to, message: message) when is_binary(from) and is_binary(to) and is_binary(message) do
+
+    body = %{
+      src: from,
+      dst: to,
+      text: message,
+      type: "sms",
+      log: true
+    } |> Poison.encode!
+
+    {:ok, %{body: resp, status_code: status_code}} = HTTPoison.post(
+      uri,
+      body,
+      [
+        {"Authorization", "Basic " <> auth},
+        {"Content-Type", "application/json"}
+      ]
+    )
+
+    {:ok, %{"api_id" => api_id, "message" => message}} = Poison.decode(resp)
+
+    %{
+      id: api_id,
+      result_string: message,
+      status_code: status_code
+    }
+  end
+
+end
