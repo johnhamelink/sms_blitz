14132a939956fd0ed2910c26edda7dd195ba1f86
John Hamelink <john@johnhamelink.com>
2017-12-12 17:04:12 +0000

    Bump version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SmsBlitz.Mixfile do
 
   def project do
     [app: :sms_blitz,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.0",
      description: "Send SMS messages through various different suppliers",
      build_embedded: Mix.env == :prod,
